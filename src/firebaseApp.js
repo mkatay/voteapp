58a284211ab8a752e9ec2b5f6dafa55af9a482f3
KMagdi <46417302+kmagdi@users.noreply.github.com>
2024-04-24 13:49:37 +0200

    fileupload

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import {firebaseConfig} from './firebaseConfig'
 import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
+import { getStorage } from "firebase/storage";
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
@@ -10,5 +11,5 @@ export const app = initializeApp(firebaseConfig);
 export const auth=getAuth(app)
 export const provider=new GoogleAuthProvider()
 export const db=getFirestore(app)
-export const storage=getFirestore(app)
+export const storage=getStorage(app)
 
--- src/uploadFile.js
+++ src/uploadFile.js
@@ -7,11 +7,15 @@ import {
 import { v4} from 'uuid';
 
 export const uploadFile = async (file) => {
+  console.log(file);
   try {
+    console.log(`projects/${v4()+file.name.slice(-4)}`)
+    
     const fileRef = ref(storage, `projects/${v4()+file.name.slice(-4)}`);
     await uploadBytes(fileRef, file);
     const photoUrl = await getDownloadURL(fileRef);
     return photoUrl;
+    return null
   } catch (err) {
     console.log("Hiba a fájlfeltöltés során:", err);
     throw err;
